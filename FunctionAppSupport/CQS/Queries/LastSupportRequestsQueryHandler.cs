cb07f5b4e8d0b26d6d4edb9605fb722feee2a5ad
Renato Groffe <renato.groffe@yahoo.com.br>
2021-12-04 06:56:14 -0300

    .NET 6 + Azure Functions + Isolated Process + HttpTrigger + MediatR + CQS (Command-query separation) + SQL Server + Entity Framework Core + Chamados de Suporte
    .NET 6 + Azure Functions + Isolated Process + HttpTrigger + MediatR + CQS (Command-query separation) + SQL Server + Entity Framework Core + Chamados de Suporte

---
+++ FunctionAppSupport/CQS/Queries/LastSupportRequestsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using FunctionAppSupport.Data;
+
+namespace FunctionAppSupport.CQS.Queries
+{
+    public class LastSupportRequestsQueryHandler :
+        IRequestHandler<LastSupportRequestsQuery, IEnumerable<LastSupportRequestsQueryResult>>
+    {
+        private readonly SupportContext _context;
+
+        public LastSupportRequestsQueryHandler(SupportContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<LastSupportRequestsQueryResult>> Handle(LastSupportRequestsQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_context.SupportRequests.OrderByDescending(r => r.Id)
+                .Take(request.NumberLastSupportRequests)
+                .Select(s => new LastSupportRequestsQueryResult()
+                {
+                    Id = s.Id,
+                    RequestDate = s.RequestDate,
+                    Email = s.Email,
+                    Problem = s.Problem
+                }).AsEnumerable());
+        }
+    }
+}
